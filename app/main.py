88dc5540ec8714bf7913549345da9482fc243139
Kasjan Michalak <kasjan.michalak@gmail.com>
2026-02-18 18:05:53 +0000

    1save

--- app/main.py
+++ app/main.py
@@ -1,1 +1,27 @@
-# write your code here
+import os
+
+
+def move_file(command: str):
+    lista = command.split()
+    if lista[0] != "mv" :
+        return
+    current_file_name = lista[1]
+    dest_path = lista[2]
+
+    if dest_path[-1] == "/":
+        new_file_name = current_file_name.split("/")[-1]
+        dest = dest_path + new_file_name
+    else:
+        dest = dest_path
+    dirs = dest.split("/")[:-1]
+    current = ""
+    for d in dirs:
+        current += d + "/"
+        if not os.path.exists(current):
+            os.mkdir(current)
+
+    with open (current_file_name, "r") as file:
+        content = file.read()
+        with open (dest, "w") as file_new:
+            file_new.write(content)
+    os.remove(current_file_name)
